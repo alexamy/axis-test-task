7a946616c8c7944348d1cfe5e8d1870cf4ea5188
alexij <achasch@gmail.com>
2017-12-22 00:13:04 +0300

    mid

--- project/axis.js
+++ project/axis.js
@@ -1,3 +1,14 @@
+var aNum = "#aNum";
+var bNum = "#bNum";
+var sumNum = "#sumNum";
+var arrowImg1 = "#arrowsImg1";
+var arrowImg2 = "#arrowsImg2";
+var arrowsPathPrefix = "images/arrow_";
+var arrowsWidth = [0,0, 86, 126, 162, 200, 240, 279, 316, 356];
+
+var aInput = "#aNumInput";
+var bInput = "#bNumInput";
+
 function generateTask()
 {
     var task = {};
@@ -7,10 +18,57 @@ function generateTask()
     return task;
 };
 
+function getArrowPath(num) {
+    if(num >= 2 && num <= 9)
+        return arrowsPathPrefix + num + ".png";
+    else
+        console.log("error at getArrowPath");
+    return "";
+}
+
+function showArrow(id, num) {
+    $(id).toggleClass("hidden");
+    $(id).attr('src', getArrowPath(num));
+    $(id).width(0);
+}
+
+var task;
 $(document).ready(function() {
-    var task = generateTask();
-    console.log(task);
-    $("#aNum").text(task.a);
-    $("#bNum").text(task.b);
-    $("#sumNum").text("?");
+    //generating task
+    task = generateTask();
+    $(aNum).text(task.a);
+    $(bNum).text(task.b);
+    $(sumNum).text("?");
+
+    showArrow(arrowImg1, task.a);
+
+    var animLength = 0 //2000 for production
+    $(arrowImg1).animate({width: arrowsWidth[task.a]}, animLength, "swing", function() {
+        //a number input
+        $(aInput).toggleClass("hidden");
+        $(aInput).css("marginLeft", arrowsWidth[task.a]/2 - 10);
+        $(aInput).keyup(function() {
+            //a number is changed
+            var entered = $(aInput).val();
+            if(entered.length == 1)
+            {
+                if(entered == task.a) {
+                    $(aNum).removeClass("wrongNum");
+                    $(aInput).removeClass("wrongInput");
+                    $(aInput).attr("readonly", 1);
+                    secondPart();
+                    $(aInput).off("change");
+                }
+                else {
+                    $(aNum).addClass("wrongNum");
+                    $(aInput).addClass("wrongInput");
+                }
+            }
+        })
+    });
 });
+
+function secondPart()
+{
+    showArrow(arrowImg2, task.b);
+}
