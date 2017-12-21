80ec700b11e46eb8f569f083275a678496cc274f
alexij <achasch@gmail.com>
2017-12-22 01:52:54 +0300

    comments, refactor

--- project/axis.js
+++ project/axis.js
@@ -1,15 +1,6 @@
-var aNum = "#aNum";
-var bNum = "#bNum";
-var sumNum = "#sumNum";
-var arrowImg1 = "#arrowsImg1";
-var arrowImg2 = "#arrowsImg2";
-var arrowsPathPrefix = "images/arrow_";
-var arrowsWidth = [0,0, 86, 126, 162, 200, 240, 279, 316, 356];
-
-var aInput = "#aNumInput";
-var bInput = "#bNumInput";
-var sumInput = "#sumNumInput";
-
+//generate table of random task
+//a: [6;9]
+//a+b: [11,14]
 function generateTask()
 {
     var task = {};
@@ -19,7 +10,9 @@ function generateTask()
     return task;
 };
 
+//returns path to arrow img for 2..9
 function getArrowPath(num) {
+    var arrowsPathPrefix = "images/arrow_";
     if(num >= 2 && num <= 9)
         return arrowsPathPrefix + num + ".png";
     else
@@ -27,41 +20,14 @@ function getArrowPath(num) {
     return "";
 }
 
+//reset page after 3 seconds
 function resetPage() {
     setTimeout(function() {
         location.reload();
     }, 3000);
 }
 
-function checkInput(inputName, answer, hintText, successFunc) {
-        //show if not shown
-        $(inputName).removeClass("hidden");
-        $(inputName).removeClass("nonExists");
-
-        //number is changed
-        $(inputName).keyup(function() {
-            var entered = $(inputName).val();
-            var maxlen = $(inputName).attr("maxlength")
-            if(entered.length == maxlen)
-            {
-                //success
-                if(entered == answer) {
-                    if(hintText !== undefined) $(hintText).removeClass("wrongNum");
-                    $(inputName).removeClass("wrongInput");
-                    $(inputName).attr("readonly", 1);
-                    if(successFunc !== undefined) successFunc();
-                    $(inputName).off("keyup");
-                }
-                //try again
-                else {
-                    if(hintText !== undefined) $(hintText).addClass("wrongNum");
-                    $(inputName).addClass("wrongInput");
-                }
-            }
-        });
-        $(inputName).focus();
-}
-
+//animating arrows, then checkInput
 function showPart(arrowImg, hintText, inputBox, answer, successFunc) {
     $(arrowImg).toggleClass("hidden");
     $(arrowImg).attr('src', getArrowPath(answer));
@@ -73,23 +39,69 @@ function showPart(arrowImg, hintText, inputBox, answer, successFunc) {
     });
 }
 
+//input handling (after arrows animate)
+//inputBox - number text input
+//answer - number value that want to match (i.e task values)
+//hintText - will be coloured yellow on wrong answer
+//successFunc - started if input is correct
+function checkInput(inputBox, answer, hintText, successFunc) {
+    //show if not shown
+    $(inputBox).removeClass("hidden");
+
+    //number is changed
+    $(inputBox).keyup(function() {
+        var entered = $(inputBox).val();
+        var maxlen = $(inputBox).attr("maxlength")
+        if(entered.length == maxlen)
+        {
+            //success
+            if(entered == answer) {
+                if(hintText !== undefined) $(hintText).removeClass("wrongNum");
+                $(inputBox).removeClass("wrongInput");
+                $(inputBox).attr("readonly", 1);
+                if(successFunc !== undefined) successFunc();
+                $(inputBox).off("keyup");
+            }
+            //try again
+            else {
+                if(hintText !== undefined) $(hintText).addClass("wrongNum");
+                $(inputBox).addClass("wrongInput");
+            }
+        }
+    });
+    $(inputBox).focus();
+}
+
+//links to page elements
+var aNumText = "#aNumText";
+var bNumText = "#bNumText";
+var sumNumText = "#sumNumText";
+var arrowImg1 = "#arrowsImg1";
+var arrowImg2 = "#arrowsImg2";
+var aInput = "#aInputBox";
+var bInput = "#bInputBox";
+var sumInput = "#sumInputBox";
+//array of arrows images widths, starting from 2 to 9
+var arrowsWidth = [0,0, 86, 126, 162, 200, 240, 279, 316, 356];
+
+//main function
 var task;
 $(document).ready(function() {
     //generating task
     task = generateTask();
-    $(aNum).text(task.a);
-    $(bNum).text(task.b);
-    $(sumNum).text("?");
+    $(aNumText).text(task.a);
+    $(bNumText).text(task.b);
+    $(sumNumText).text("?");
 
-    //set css (offsets, etc) based on task
+    //set css based on task
     $(aInput).css("marginLeft", arrowsWidth[task.a]/2 - 10);
     $(bInput).css("marginLeft", arrowsWidth[task.a]/2 + arrowsWidth[task.b]/2 - 35);
 
     //actual task work
-    showPart(arrowImg1, aNum, aInput, task.a, function() {
-        showPart(arrowImg2, bNum, bInput, task.b, function() {
-            $(sumNum).toggleClass("nonExists");
-            checkInput(sumNumInput, task.sum, undefined, resetPage);
+    showPart(arrowImg1, aNumText, aInput, task.a, function() {
+        showPart(arrowImg2, bNumText, bInput, task.b, function() {
+            $(sumNumText).remove();
+            checkInput(sumInputBox, task.sum, undefined, resetPage);
         })
     });
 });
